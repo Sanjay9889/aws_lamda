275e1b79763357c9fec308f80210e2daae521821
Sanjay Maurya <sanjaymaurya9889@gmail.com>
2021-06-30 13:43:07 +0530

    build pipeline

--- lambda_function.py
+++ lambda_function.py
@@ -9,6 +9,9 @@ def lambda_handler(event, context):
         "age": [30, 28, 23]
     }
     df = pd.DataFrame(data)
+    print("*" * 50)
+    print(df)
+    print("*" * 50)
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!'),
